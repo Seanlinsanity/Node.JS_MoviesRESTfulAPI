6d650cc35ae617df83c64130661093f8a77af6f9
Sean Lin <seanlin1016@gmail.com>
2018-07-16 11:04:27 +0800

    User Router and Authentication

---
+++ routes/auth.js
@@ -0,0 +1,31 @@
+const bcrypt = require('bcrypt')
+const _ = require('lodash')
+const Joi = require('Joi')
+const {User} = require('../models/user');
+const mongoose = require('mongoose');
+const express = require('express');
+const router = express.Router();
+
+router.post('/', async (req, res) => {
+    const { error } = validate(req.body); 
+    if (error) return res.status(400).send(error.details[0].message);
+
+    let user = await User.findOne({ email: req.body.email })
+    if (!user) return res.status(400).send('Invalid email or password')
+
+    const validPassword = await bcrypt.compare(req.body.password, user.password)
+    if (!validPassword) return res.status(400).send('Invalid email or password')
+
+    res.send(true)
+});
+
+function validate(req) {
+    const schema = {
+      password: Joi.string().min(5).max(1024).required(),
+      email: Joi.string().min(5).max(255).required().email()
+    };
+  
+    return Joi.validate(req, schema);
+  }
+
+module.exports = router
